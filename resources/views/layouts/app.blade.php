0fb09e9dbc0b053a364968a4ff7ec8058cde8c13
Dmitry Myakishev <dm12se@gmail.com>
2021-06-20 17:43:36 +0300

    commit 16 - кнопка выйти

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -100,6 +100,13 @@
                                             <a class="nav-link" href="/account/problems">
                                                 Мои заявки
                                             </a>
+                                            <a class="nav-link" href="{{ route('logout') }}"
+                                               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                                Выйти из системы
+                                            </a>
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                @csrf
+                                            </form>
                                         </li>
                                     </ul>
                                 </div>
